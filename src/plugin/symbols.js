0684e2ef26ec63b7abe369216f7c880c7fccb52d
wangbo <18623213198@163.com>
2020-11-21 21:32:59 +0800

    update: symbols icon

--- src/plugin/symbols.js
+++ src/plugin/symbols.js
@@ -1,13 +1,19 @@
 /**
  * @Author hanwei
  * @Email wb846022604@gamil.com
- * @LastAuthor hanwei
+ * @LastAuthor wangbo
  * @CreatedTime 2020-11-21 18:33:20
- * @LastModifyTime 2020-11-21 18:54:00
+ * @LastModifyTime 2020-11-21 21:32:33
  */
+
+const chalk = require("chalk")
 
 module.exports = {
   success: '✅',
-  info: '👉',
-  error: '❌'
+  hand_right: '👉',
+  error: '❌',
+  info: chalk.blue('ℹ'),
+	// success: chalk.green('✔'),
+	warning: chalk.yellow('⚠'),
+	// error: chalk.red('✖')
 }
