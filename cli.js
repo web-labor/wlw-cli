d54f7b102cc9c5ce157c593d5fa34004629e9c0a
wangbo <18623213198@163.com>
2020-11-21 21:24:13 +0800

    add: cmd description and package.version

--- cli.js
+++ cli.js
@@ -1,25 +1,30 @@
 /**
  * @Author hanwei
  * @Email wb846022604@gamil.com
- * @LastAuthor hanwei
+ * @LastAuthor wangbo
  * @CreatedTime 2020-11-21 15:51:51
- * @LastModifyTime 2020-11-21 20:31:23
+ * @LastModifyTime 2020-11-21 21:22:23
  */
 // #!/usr/bin/env node
 // --这种用法是为了防止操作系统用户没有将node装在默认的/usr/bin路径里。当系统看到这一行的时候，
 // 首先会到env设置里查找node的安装路径，再调用对应路径下的解释器程序完成操作。
 const program = require("commander")
+const packageInfo = require('./package.json');
 
 const cmdInit = require('./src/cmd/init')
 const cmdList = require('./src/cmd/list')
+
 program
-  .version("0.0.1", "-v, --version")
+    .version(packageInfo.version)
+
+program
   .command("init <name>")
+  .description('初始化模版')
   .action(cmdInit)
 
 program
-  .version("0.0.1", "-v, --version")
   .command("list")
+  .description('查看模版列表')
   .alias('ls') // 简写
   .action(cmdList)
 
